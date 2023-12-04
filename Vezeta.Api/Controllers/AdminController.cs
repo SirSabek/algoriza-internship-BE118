aab16fd1dbc8543568b5e59e02bfda34300aa9e9
SirSabek <mmsabek99@gmail.com>
2023-12-05 01:54:38 +0200

    adding Admin functions

--- Vezeta.Api/Controllers/AdminController.cs
+++ Vezeta.Api/Controllers/AdminController.cs
@@ -1,8 +1,11 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Vezeta.Application.Common;
 using Vezeta.Application.Common.Interfaces.Persistance;
 using Vezeta.Application.Services.Authentication;
+using Vezeta.Contract.Dtos.Admin;
 using Vezeta.Contract.Authentication;
+using Vezeta.Domain.Entities;
 
 namespace Vezeta.Api.Controllers;
 
@@ -12,10 +15,12 @@ namespace Vezeta.Api.Controllers;
 public class AdminController : ControllerBase
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
 
-    public AdminController(IUnitOfWork unitOfWork)
+    public AdminController(IUnitOfWork unitOfWork, IMapper mapper)
     {
         _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
 
     //Doctors
@@ -23,39 +28,74 @@ public class AdminController : ControllerBase
     [HttpGet("doctors")]
     public async Task<IActionResult> GetDoctors([FromQuery] RequestParams requestParams)
     {
-        var doctors = await _unitOfWork.Doctors.GetPagedList(requestParams);
-
-        //map to dto
-        return Ok(doctors);
+        var doctors = await _unitOfWork.Doctors.GetPagedList(requestParams, new List<string> { "Specialization" });
+        var result = _mapper.Map<List<GetDoctorDto>>(doctors);
+        return Ok(result);
     }
 
-
-    [HttpGet("id:int", Name = "GetDoctor")]
+    [HttpGet("doctor/{id:int}")]
     public async Task<IActionResult> GetDoctor(int id)
     {
-        var doctor = await _unitOfWork.Doctors.GetById(id);
-
-        //map to dto
-        return Ok(doctor);
+        var doctor = await _unitOfWork.Doctors.Get(q => q.Id == id, new List<string> { "Specialization" });
+        var result = _mapper.Map<GetDoctorDto>(doctor); 
+        return Ok(result);
     }
 
     [HttpPost("AddDoctor")]
-    public async Task<IActionResult> AddDoctor()
+    public async Task<IActionResult> AddDoctor([FromBody] AddDoctorDto doctorDto)
     {
+        var doctor = _mapper.Map<Doctor>(doctorDto);
+        await _unitOfWork.Doctors.Insert(doctor);
+        await _unitOfWork.Save();
+        //var result = _mapper.Map<GetDoctorDto>(doctor);
+        return Ok(doctorDto);
+    }
 
+    [HttpPut("id:int", Name ="UpdateDoctor")]
+    public async Task<IActionResult> UpdateDoctor(int id, [FromBody] UpdateDoctorDto doctorDto)
+    {
+        var doctor = await _unitOfWork.Doctors.Get(q => q.Id == id);
+        doctor = _mapper.Map(doctorDto, doctor);
+        _unitOfWork.Doctors.Update(doctor);
+        await _unitOfWork.Save();
+        return NoContent();
+    }
+
+    [HttpDelete("DeleteDoctor")]
+    public async Task<IActionResult> DeleteDoctor(int id)
+    {
+        await _unitOfWork.Doctors.Delete(id);
+        await _unitOfWork.Save();
+        return NoContent();
+    }
+
+    //Patients
+    [HttpGet("patients")]
+    public async Task<IActionResult> GetPatients([FromQuery] RequestParams requestParams)
+    {
+        var patients = await _unitOfWork.Patients.GetPagedList(requestParams, new List<string> { "Bookings","Invoices" });
+        var result = _mapper.Map<List<GetPatientDto>>(patients);
+        return Ok(result);
+    }
+
+    [HttpGet("patient/{id:int}")]
+    public async Task<IActionResult> GetPatient(int id)
+    {
+        var patient = await _unitOfWork.Patients.Get(q => q.Id == id, new List<string> { "Bookings", "Invoices" });
+        var result = _mapper.Map<GetPatientDto>(patient);
+        return Ok(result);
     }
 
 
-
-
-
-
-
-
-
-
-
-
-    //Patients
     //Dashboard
+
+    //NumOfDoctors
+
+    //NumOfPatients
+
+    //NumOfBookings
+
+    //Top 5 Specializations
+
+    //Top 10 Doctors
 }
