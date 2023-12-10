c194514ad4cde85f7152f46702a64537493f12ea
SirSabek <mmsabek99@gmail.com>
2023-12-10 15:35:21 +0200

    removing unwanted service

--- Vezeta.Application/DependencyInjection.cs
+++ Vezeta.Application/DependencyInjection.cs
@@ -1,5 +1,4 @@
 using Microsoft.Extensions.DependencyInjection;
-using Vezeta.Application.Services.Authentication;
 using Vezeta.Application.Services.Authentication.Auth;
 
 namespace Vezeta.Application;
@@ -8,7 +7,6 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddScoped<IAuthenticationService,AuthenticationService>();
         services.AddScoped(typeof(IAuthManager<>), typeof(AuthManager<>) );
 
         return services;
