950ac7dabdf5a7075c90d7066551933a142d0be1
SirSabek <mmsabek99@gmail.com>
2023-12-10 15:33:02 +0200

    updating Auth service

--- Vezeta.Application/Services/Authentication/AuthenticationResult.cs
+++
@@ -1,8 +0,0 @@
-using Vezeta.Domain.Entities;
-
-namespace Vezeta.Application.Services.Authentication;
-
-public record AuthenticationResult (
-    User User,
-    string Token
-);
--- Vezeta.Application/Services/Authentication/IAuthenticationService.cs
+++
@@ -1,7 +0,0 @@
-namespace Vezeta.Application.Services.Authentication;
-
-public interface IAuthenticationService
-{
-    Task<AuthenticationResult> RegisterAsync(string firstName, string lastName, string email, string password);
-    Task<AuthenticationResult> LoginAsync(string email, string password );
-}
